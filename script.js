b1c0fcacb89e1f097e09e5e2f97ac4ed1d9bdcc1
luisalves <lg.alvesz11@gmail.com>
2024-10-24 19:20:01 -0300

    Adicionando mais conteúdos no script

--- script.js
+++ script.js
@@ -29,17 +29,80 @@ const showSuccess = (input, message) => {
     
 }
 
-const checkUsername = () => {
+const isRequired = value => value === '' ? false : true;
 
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+const isPasswordSecure = (senha) => {
+    const re = new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+    return re.test(senha);
+}
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+const checkUsername = () => {
+    let valid = false;
+    const min = 3;
+    const max = 25;
+    const usuarioVal = usuario.valid.trim();
+
+    if (!isRequired(usuarioVal)) {
+        showError(usuario, 'Usuário não pode ficar em branco.');
+    } else if (!isBetween(usuarioVal.lenght, min, max)) {
+        showError(usuario, 'Usuário deve ter entre ${min} e ${max} caracteres');
+    } else {
+        showSuccess(usuario);
+        valid = true;
+    }
+    return valid;
 }
 
 const checkEmail = () => {
-    
+    let valid = false;
+
+    const emailVal = email.value.trim();
+
+    if (!isRequired(emailVal)) {
+        showError(email, 'E-mail não pode ficar em branco.');
+    } else if (!isEmailValid(emailVal)) {
+        showError(email, 'E-mail inválido.');
+    } else {
+        showSuccess(email);
+        valid = true;
+    }
+    return valid;
 }
 
 const checkPassword = () => {
-    
+    let valid = false;
+
+    const senhaVal = senha.value.trim();
+
+    if (!isRequired(senhaVal)) {
+        showError(senha, 'Senha não pode ficar em branco.');
+    } else if (!isPasswordSecure(emailVal)) {
+        showError(senha, 'Senha tem que ter no mínimo 8 caracteres que incluem pelo menos 1 caractere minúsculo, 1 caractere maiúsculo, 1 número e 1 caractere especial (!@#$%^&*)');
+    } else {
+        showSuccess(senha);
+        valid = true;
+    }
+    return valid;
 }
+
+toggleSenha.addEventListener("click", function () {
+    const type = senha.getAttribute("type") === "password" ? "text" : "password";
+    senha.setAttribute("type", type);
+    this.classList.toggle("bi-eye");
+});
+
+toggleConfirmacaoSenha.addEventListener("click", function () {
+    const type = confirmacaoSenha.getAttribute("type") === "password" ? "text" : "password";
+    confirmacaoSenha.setAttribute("type", type);
+    this.classList.toggle("bi-eye");
+});
 
 const checkConfirmPassword = () => {
     
